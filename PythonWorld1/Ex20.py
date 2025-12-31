8cf1cd8859beb5ffaddaa7913fdd4147faf5d7be
Lucas Melo | Developer <lucasandradedemelo377@gmail.com>
2025-12-30 22:35:53 -0300

    feat: add program to shuffle students presentation order

---
+++ PythonWorld1/Ex20.py
@@ -0,0 +1,14 @@
+#O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+from random import shuffle
+
+student1 = input("Enter the name of student 1: ")
+student2 = input("Enter the name of student 2: ")
+student3 = input("Enter the name of student 3: ")
+student4 = input("Enter the name of student 4: ")
+
+students = [student1, student2, student3, student4]
+shuffle(students)
+
+print("Presentation order:")
+print(students)
