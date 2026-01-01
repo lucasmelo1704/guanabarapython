c03080c1b5f1845c00f94212b45ad864d7304cd3
Lucas Melo <lucasandradedemelo377@gmail.com>
2026-01-01 19:47:59 -0300

    feat: implement mp3 file execution using os.startfile on Windows

---
+++ PythonWorld1/Ex21.py
@@ -0,0 +1,7 @@
+#Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+
+import os
+
+file_path = r"C:\Users\lucas\Downloads\GitHub\guanabarapython\Arquivos\music.mp3"
+
+os.startfile(file_path)
